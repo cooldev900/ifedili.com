fe0a36c64c0c1fc3023a214c28edac3ae4c62d41
cooldev900 <cooldev900@gmail.com>
2025-05-16 14:26:48 -0400

    fix image error

--- src/app/core/data/tools.ts
+++ src/app/core/data/tools.ts
@@ -3,151 +3,151 @@ import { ITool } from '../models';
 export default {  
   react: {
     id: "react",
-    image: "./../../../../assets/skills/react.png",
+    image: "assets/skills/react.png",
     name: "ReactJS",
     categories: ["Web"]
   },
   ang: {
     id: "ang",
-    image: "./../../../../assets/skills/angular.png",
+    image: "assets/skills/angular.png",
     name: "Angular",
     categories: ["Web"]
   },
   nod: {
     id: "nod",
-    image: "./../../../../assets/skills/nodejs.png",
+    image: "assets/skills/nodejs.png",
     name: "NodeJS",
     categories: ["Web"]
   },
   ex: {
     id: "ex",
-    image: "./../../../../assets/skills/express.png",
+    image: "assets/skills/express.png",
     name: "ExpressJS",
     categories: ["Web"]
   },
   ionic: {
     id: "ionic",
-    image: "./../../../../assets/skills/ionic.png",
+    image: "assets/skills/ionic.png",
     name: "Ionic",
     categories: ["Others"]
   },
   ts: {
     id: "ts",
-    image: "./../../../../assets/skills/typescript.svg",
+    image: "assets/skills/typescript.svg",
     name: "TypeScript",
     categories: ["Languages"]
   },
   js: {
     id: "js",
-    image: "./../../../../assets/skills/javascript.png",
+    image: "assets/skills/javascript.png",
     name: "JavaScript",
     categories: ["Languages"]
   },
   sol: {
     id: "sol",
-    image: "./../../../../assets/skills/solidity.png",
+    image: "assets/skills/solidity.png",
     name: "Solidity",
     categories: ["Languages", "Blockchain"]
   },
   go: {
     id: "go",
-    image: "./../../../../assets/skills/golang.png",
+    image: "assets/skills/golang.png",
     name: "Go",
     categories: ["Languages"]
   },
   mongo: {
     id: "mongo",
-    image: "./../../../../assets/skills/mongodb.png",
+    image: "assets/skills/mongodb.png",
     name: "MongoDB",
     categories: ["Web"]
   },
   postg: {
     id: "postg",
-    image: "./../../../../assets/skills/postgre.png",
+    image: "assets/skills/postgre.png",
     name: "PostgreSQL",
     categories: ["Web"]
   },
   redis: {
     id: "redis",
-    image: "./../../../../assets/skills/redis.png",
+    image: "assets/skills/redis.png",
     name: "Redis",
     categories: ["Web"]
   },
   fbase: {
     id: "fbase",
-    image: "./../../../../assets/skills/firebase.png",
+    image: "assets/skills/firebase.png",
     name: "Firebase",
     categories: ["Web"]
   },
   aws: {
     id: "aws",
-    image: "./../../../../assets/skills/aws.png",
+    image: "assets/skills/aws.png",
     name: "AWS",
     categories: ["Web"]
   },
   html: {
     id: "html",
-    image: "./../../../../assets/skills/html.png",
+    image: "assets/skills/html.png",
     name: "HTML",
     categories: ["Languages"]
   },
   css: {
     id: "css",
-    image: "./../../../../assets/skills/css.png",
+    image: "assets/skills/css.png",
     name: "CSS",
     categories: ["Languages"]
   },
   scss: {
     id: "scss",
-    image: "./../../../../assets/skills/scss.png",
+    image: "assets/skills/scss.png",
     name: "SCSS",
     categories: ["Web"]
   },
   gsap: {
     id: "gsap",
-    image: "./../../../../assets/skills/gsap.svg",
+    image: "assets/skills/gsap.svg",
     name: "GSAP",
     categories: ["Web"]
   },
   dock: {
     id: "dock",
-    image: "./../../../../assets/skills/docker.png",
+    image: "assets/skills/docker.png",
     name: "Docker",
     categories: ["Others"]
   },
   nest: {
     id: "nest",
-    image: "./../../../../assets/skills/nestjs.png",
+    image: "assets/skills/nestjs.png",
     name: "NestJS",
     categories: ["Web"]
   },
   sql: {
     id: "sql",
-    image: "./../../../../assets/skills/sql.png",
+    image: "assets/skills/sql.png",
     name: "SQL",
     categories: ["Languages"]
   },
   ejs: {
     id: "ejs",
-    image: "./../../../../assets/skills/ejs.jpeg",
+    image: "assets/skills/ejs.jpeg",
     name: "EJS",
     categories: ["Web"]
   },
   php: {
     id: "php",
-    image: "./../../../../assets/skills/php.png",
+    image: "assets/skills/php.png",
     name: "PHP",
     categories: ["Languages"]
   },
   psd: {
     id: "psd",
-    image: "./../../../../assets/skills/photoshop.png",
+    image: "assets/skills/photoshop.png",
     name: "Photoshop",
     categories: ["Design"]
   },
   figma: {
     id: "figma",
-    image: "./../../../../assets/skills/figma.png",
+    image: "assets/skills/figma.png",
     name: "Figma",
     categories: ["Design"]
   },
